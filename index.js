f0dc2d7a1d3e1b578f2e8a989a751f5cfdb71b9d
Nischey Gupta <ngupta16@my.bcit.ca>
2022-05-11 00:05:52 +0530

    modified index.js and package.json

--- index.js
+++ index.js
@@ -44,21 +44,21 @@ app.use(session({
 // })
 
 
-// middleware that is specific to this router
-router.use((req, res, next) => {
-    console.log('Time: ', Date.now())
-    next()
-})
-// define the home page route
-router.get('/', (req, res) => {
-    res.send('Birds home page')
-})
-// define the about route
-router.get('/about', (req, res) => {
-    res.send('About birds')
-})
+// // middleware that is specific to this router
+// router.use((req, res, next) => {
+//     console.log('Time: ', Date.now())
+//     next()
+// })
+// // define the home page route
+// router.get('/', (req, res) => {
+//     res.send('Birds home page')
+// })
+// // define the about route
+// router.get('/about', (req, res) => {
+//     res.send('About birds')
+// })
 
-module.exports = router
+// module.exports = router
 
 
 
