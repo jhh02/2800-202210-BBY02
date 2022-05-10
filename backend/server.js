e881d9144096222209ee19ca2c42b691de4a9531
Jun Han <junhohan2@gmail.com>
2022-05-10 12:15:01 -0700

    new test/experiment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 
 connectDB()
 
@@ -28,6 +28,6 @@ app.use('/api/users', require('./routes/userRoutes'))
 //     app.get('/', (req, res) => res.send('Please set to production'))
 // }
 
-app.use(errorHandler)
+app.use(errorHandl222222222er)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
