70b350720a92127615fc06398a5e5028201935f5
Jun Han <junhohan2@gmail.com>
2022-05-12 11:05:53 -0700

    register login tested

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,78 +1,9 @@
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
-// @desc    Register new user
-// @route   POST /api/users
-// @access  Public
-const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password, address, role } = req.body
-    if (!name || !email || !password) {
-        res.status(400)
-        throw new Error('Please add all fields')
-    }
 
-    // Check if user exists
-    const userExists = await User.findOne({ email })
-
-    if (userExists) {
-        res.status(400)
-        throw new Error('User already exists')
-    }
-
-    // Create user
-    const user = await User.create({
-        name,
-        email,
-        password,
-        address,
-        role,
-        admin,
-    })
-    if (user) {
-        res.status(201).json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-            password: user.password,
-            address: user.address,
-            role: user.role,
-            admin: user.admin,
-            // token: generateToken(user._id),
-        })
-        await user.save()
-    } else {
-        res.status(400)
-        throw new Error('Invalid user data')
-    }
-})
-
-// @desc    Authenticate a user
-// @route   POST /api/users/login
-// @access  Public
-const loginUser = asyncHandler(async (req, res) => {
-    const { email, password, isAdmin, role } = req.body
-
-    // Check for user email
-    const user = await User.findOne({ email })
-
-    //check for admin
-    if (user && user.isAdmin && await bcrypt.compare(password, user.password)) {
-        res.render('dashboard')
-    } else if (user && (await bcrypt.compare(password, user.password))) {
-        res.json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-            // token: generateToken(user._id),
-        })
-    } else {
-        res.status(400)
-        throw new Error('Invalid credentials')
-    }
-})
 
 // @desc    Get user data
 // @route   GET /api/users/me
@@ -89,7 +20,6 @@ const generateToken = (id) => {
 }
 
 module.exports = {
-    registerUser,
-    loginUser,
+
     getMe,
 }
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,31 +2,28 @@ const express = require('express')
 const { render } = require('express/lib/response')
 const router = express.Router()
 const {
-    registerUser,
-    loginUser,
     getMe,
 } = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
 
-router.get('/', (req, res) => {
-    res.render('register')
-})
+// router.get('/', (req, res) => {
+//     // res.render('register')
+// })
 
-router.get('/register', (req, res) => {
-    res.render('register')
-})
-
-router.post('/register', registerUser)
-
-router.get('/login', (req, res) => {
-    res.render('login')
-})
-
-router.post('/login', loginUser)
+// router.post('/register', registerUser, (req, res) => {
+//     res.render()
+// })
+// router.get('/register', (req, res) => {
+//     res.render('register')
+// })
 
 
-// router.post('/', registerUser)
-// router.post('/', registerUser)
+// router.get('/login', (req, res) => {
+//     res.render('login')
+// })
+
 // router.post('/login', loginUser)
+
+
 
 module.exports = router
