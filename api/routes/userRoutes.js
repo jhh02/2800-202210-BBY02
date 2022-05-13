afed913883b8be871e82f60d916c256f1efc7e60
Jun Han <junhohan2@gmail.com>
2022-05-13 16:33:32 -0700

    admin

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const objectId = require('mongodb').ObjectId
 const { render } = require('express/lib/response')
-const { registerUser, loginUser, getMe, editUser } = require('../controllers/userController')
-const { addAUser } = require('../controllers/adminController')
+const { registerUser, loginUser, getMe } = require('../controllers/userController')
+const { addAUser, editAUser } = require('../controllers/adminController')
 const router = express.Router()
 const { protect } = require('../middleware/authMiddleware')
 const User = require('../models/userModel')
+const { db } = require('../models/userModel')
 
 router.get('/', (req, res) => {
     res.render('register')
@@ -20,38 +22,63 @@ router.get('/logout', (req, res) => {
     res.render('login')
 })
 
-router.get('/profile', (req, res) => {
-    res.render('profile')
+router.get('/profile/:id', (req, res) => {
+    const id = req.params.id
+    User.findById(id)
+        .then(result => {
+            res.render('profile', { user: result })
+        })
 })
 
-router.get('/edit', (req, res) => {
-    res.render('edit')
+router.post('/profile/:id', (req, res) => {
+    res.render('user-edit')
 })
-// router.post('/edit', editUser)
 
 
-
-
-router.get('/getme', protect, getMe)
+// router.get('/getme', protect, getMe)
 
 router.get('/dashboard', (req, res) => {
-    // res.render('dashboard')
     User.find({}).exec(function (err, users) {
         if (err) throw err;
         res.render('dashboard', { "users": users })
     })
 })
 
+router.get('/dashboard/adduser', (req, res) => {
+    res.render('addUser')
+})
+
+router.post('/dashboard/adduser', addAUser)
 
 router.get('/dashboard/:id', (req, res) => {
     const id = req.params.id
-    User.findById(id).exec(function (err, users) {
-        if (err) throw err;
-        res.render('user-details', { "user": users })
-    })
-
+    User.findById(id)
+        .then(result => {
+            res.render('user-details', { user: result })
+        })
 })
 
+// router.get('/dashboard/:id', (req, res) => {
+//     const id = req.params.id
+//     User.findById(id)
+//         .then(result => {
+//             res.render('updateUser', { user: result })
+//         })
+// })
+
+router.patch('/dashboard/:id', editAUser)
+
+router.delete('/dashboard/:id', async (req, res) => {
+    const id = req.params.id
+
+    User.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/user/dashboard' })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
 
 
 router.get('/dashboard/adduser', (req, res) => {
