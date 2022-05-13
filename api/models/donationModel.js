7d4ef2f1cc02877b090f0953fb83f371b9cfcc6f
Jun Han <junhohan2@gmail.com>
2022-05-12 23:30:21 -0700

    added css files

---
+++ api/models/donationModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const donationSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        required: [true, 'Please add a title']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description of an item']
+    },
+
+
+
+},
+    { timestamps: true }
+)
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,19 +2,27 @@ const express = require('express')
 const { render } = require('express/lib/response')
 const { registerUser, loginUser, getMe } = require('../controllers/userController')
 const router = express.Router()
-const {
-} = require('../controllers/userController')
+const { protect } = require('../middleware/authMiddleware')
 
 router.get('/', (req, res) => {
     res.render('register')
 })
 router.post('/', registerUser)
+
 router.get('/login', (req, res) => {
     res.render('login')
 })
 router.post('/login', loginUser)
-router.post('/getme', getMe)
+
+router.get('/logout', (req, res) => {
+    res.render('login')
+})
 
 
+router.get('/getme', protect, getMe)
+
+router.get('/dashboard', protect, (req, res) => {
+    res.render('dashboard')
+})
 
 module.exports = router
