7a3a23c7fc1d646dc035780a4e0b126e572fe478
Nischey Gupta <ngupta16@my.bcit.ca>
2022-05-13 21:00:05 +0530

    deleted back-end files

--- backend/routes/goalRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getGoal, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
-
-router.route('/').get(getGoal).post(setGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoal)
-
-module.exports = router
