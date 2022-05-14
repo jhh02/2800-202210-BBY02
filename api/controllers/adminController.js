ab6d929e4c056b090004ceb063a030f742439a4d
Jun Han <junhohan2@gmail.com>
2022-05-13 21:48:13 -0700

    cleaned dead code

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -2,9 +2,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
-const AddUsers = require('../models/adduserModel')
-
-
+const User = require('../models/user2Model')
 
 const addAUser = asyncHandler(async (req, res) => {
     const { name, email, password, address, role, isAdmin } = req.body
@@ -15,19 +13,19 @@ const addAUser = asyncHandler(async (req, res) => {
     }
 
     // Check if user exists
-    const userExists = await AddUsers.findOne({ email })
+    const userExists = await User.findOne({ email })
 
     if (userExists) {
         res.status(400)
         throw new Error('User already exists')
     }
 
-    // Hast password
+    // Hash password
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
 
     // Create user 
-    const user = await AddUsers.create({
+    const user = await User.create({
         name,
         email,
         password: hashedPassword,
@@ -60,10 +58,10 @@ const addAUser = asyncHandler(async (req, res) => {
 const editAUser = asyncHandler(async (req, res) => {
     try {
         const id = req.params.id
-        const user = await AddUsers.findById(id)
+        const user = await User.findById(id)
         const { name, email, password, address, role, isAdmin } = req.body
 
-        const editedUser = await AddUsers.findByIdAndUpdate(id, { name, email, password, address, role, isAdmin }, {
+        const editedUser = await User.findByIdAndUpdate(id, { name, email, password, address, role, isAdmin }, {
             new: true
         })
             .then(result => {
--- api/server.js
+++ api/server.js
@@ -12,10 +12,9 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 8000
 
-
+//connect to mongodb
 connectDB()
-
-
+//initialize express
 const app = express()
 //register view engine
 app.set('view engine', 'ejs')
@@ -37,91 +36,17 @@ app.use('/css', express.static('public/css'))
 app.use('/fonts', express.static('public/fonts'))
 app.use('/img', express.static('public/img'))
 app.use('/js', express.static('public/js'))
-
-//routes
-const userRoutes = require('./routes/userRoutes')
-// const donationRoutes = require('./routes/donationRoutes')
-
 //serving public files
 app.use(cookieParser())
 
+//routes
+const userRoutes = require('./routes/userRoutes')
+const rootRoutes = require('./routes/root')
+// const donationRoutes = require('./routes/donationRoutes')
 
-app.use('/', require('./routes/root'))
+app.use('/', rootRoutes)
 app.use('/user', userRoutes)
 // app.use('/donation', donationRoutes)
-
-
-// app.post('/users', async (req, res) => {
-//     try {
-//         const salt = await bcrypt.genSalt()
-//         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-//         // console.log(salt);
-//         // console.log(hashedPassword);
-
-//         const user = { name: req.body.name, password: hashedPassword }
-//         users.push(user)
-//         res.status(201).send(user)
-//     } catch (error) {
-//         res.status(500).send()
-//     }
-
-//     // hash('password') // encrypt pw
-//     // hash(salt + 'password') //dddd
-//     // hash(salt2 + 'password')
-// })
-
-// app.post('/users/login', async (req, res) => {
-//     // copy 
-//     const user = users.find(user => user.name = req.body.name)
-//     // no user
-//     if (!user) {
-//         return res.status(400).send('Cannot find user')
-//     }
-//     try {
-//         if (await bcrypt.compare(req.body.password, user.password)) {
-//             res.send('Success')
-//         } else {
-//             res.send('Not Allowed')
-//         }
-//     } catch {
-//         res.status(500).send()
-//     }
-// })
-
-// app.get('/posts', (req, res) => {
-//     res.json(posts)
-// })
-
-
-// app.get('/', (req, res) => {
-//     res.render('index')
-// })
-
-// app.get('/register', (req, res) => {
-//     res.render('register')
-// })
-
-// app.post('/register', async (req, res) => {
-//     // res.send(req.body);
-//     try {
-//         const hashedPassword = bcrypt.hash(req.body.password, 10)
-
-//         res.redirect('/login')
-//     } catch (error) {
-//         res.redirect('/register')
-//     }
-//     console.log(users);
-// })
-
-// app.get('/login', (req, res) => {
-//     //authenticate user
-//     res.render('login')
-// })
-
-
-
-
-
 
 //handles error
 app.use(errorHandler)
